7b9f0a174a062e913d6f92f3db48bd0933e1725a
xiao_wine <xiaowine0@gmailc.om>
2023-03-06 15:38:46 +0800

    杂七杂八优化

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,25 +4,10 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 22
-        targetSdk = 32
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro", "proguard-log.pro"))
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.majorVersion
     }
     namespace = "app.xiaowine.xtoast"
 }
