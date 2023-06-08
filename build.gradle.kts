5b32feabab5d6f133040fd1ce1219a540e02214d
xiaowine <xiaowine0@gmail.com>
2023-06-08 13:05:56 +0800

    add namespace

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,17 @@ plugins {
 
 android {
     compileSdk = 33
-
+    namespace = "app.xiaowine.xtoast"
     defaultConfig {
         minSdk = 22
     }
-    namespace = "app.xiaowine.xtoast"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.majorVersion
+    }
 }
 
 dependencies {}
